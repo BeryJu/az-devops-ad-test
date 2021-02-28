3d0e2e2c0c3c0abbac6dc46deb929c00251381fc
Langhammer, Jens <jens.langhammer@haufe-lexware.com>
2021-02-28 14:10:09 +0100

    fix keys to be uppercase

--- az-do-set-branch.py
+++ az-do-set-branch.py
@@ -1,15 +1,12 @@
 import os
 
-env_pr_branch = "System.PullRequest.SourceBranch"
-default_branch = "Build.SourceBranchName"
-
-print(os.environ)
+env_pr_branch = "SYSTEM_PULLREQUEST_SOURCEBRANCH"
+default_branch = "BUILD_SOURCEBRANCHNAME"
 
 branch_name = ""
 if env_pr_branch in os.environ:
     branch_name = os.environ[env_pr_branch].replace("/", "-")
 
-branch_name = os.environ[default_branch].replace("refs/heads/", "")
-
+branch_name = os.environ[default_branch]
 
 print("##vso[task.setvariable variable=branchName]%s" %  branch_name)
