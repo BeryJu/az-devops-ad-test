06d9f793528ae26e13a9cf525a9815f38dda1434
Langhammer, Jens <jens.langhammer@haufe-lexware.com>
2021-02-28 14:07:47 +0100

    debug

--- az-do-set-branch.py
+++ az-do-set-branch.py
@@ -3,10 +3,13 @@ import os
 env_pr_branch = "System.PullRequest.SourceBranch"
 default_branch = "Build.SourceBranchName"
 
+print(os.environ)
+
 branch_name = ""
 if env_pr_branch in os.environ:
     branch_name = os.environ[env_pr_branch].replace("/", "-")
 
 branch_name = os.environ[default_branch].replace("refs/heads/", "")
+
 
 print("##vso[task.setvariable variable=branchName]%s" %  branch_name)
